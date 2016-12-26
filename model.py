2c34f37a4a8b43dccca102cbe48f5e5f69bcb0c5
Benjamin Bolte <bkbolte18@gmail.com>
2016-12-25 21:15:09 -0700

    added spike histogram visualization

--- model.py
+++ model.py
@@ -9,9 +9,30 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
 import utils
 
 
+def spike_hist():
+    """Generates a histogram of spike counts over all the data."""
+
+    import matplotlib.pyplot as plt
+
+    l = list()
+    for _, spikes in utils.get_data_set('train'):
+        x = np.reshape(spikes, (-1,))
+        x = x[np.isnan(x) == False]
+        print(x.shape)
+        l.append(x)
+    c = np.concatenate(l, 0)
+
+    plt.hist(np.cast[np.int32](c), 6, log=True)
+    plt.show()
+
+
 if __name__ == '__main__':
+    m = 0
     for i, (calcium, spikes, last_spike) in enumerate(utils.generate_training_set()):
-        print(i, calcium.shape, spikes.shape, last_spike.shape)
+        # print(i, calcium.shape, spikes.shape, last_spike.shape)
+        m = max(m, int(last_spike))
+        print(i, m)
